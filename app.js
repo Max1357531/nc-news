6cd40b53c099d1e885e38f30433f252e1f1bb001
Max1357531 <max.kilburn99@gmail.com>
2025-03-12 10:33:30 +0000

    add implementation:

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const db = require('./db/connection')
 const format = require('pg-format');
 const endpoints = require('./endpoints.json')
 
-const{errors:{customErrorResponse,error500,error404},topics} = require ('./controllers')
+const{errors:{customErrorResponse,error500,error404},topics} = require ('./controllers');
+const { getTopics } = require('./controllers/topics');
 
 
 app.use(express.json())
@@ -13,9 +14,7 @@ app.get('/api',(request,response)=>{
     response.status(200).send({endpoints})
 })
 
-app.get('/api/topics',(request,response)=>{
-    response.status(200).send({endpoints})
-})
+app.get('/api/topics',getTopics)
 
 app.get('*',error404)
 app.use(customErrorResponse)
---
+++ controllers/topics.js
@@ -0,0 +1,16 @@
+const { getQueryPerm } = require("../queries")
+const { selectAllTopics } = require("../models/topics")
+
+exports.getTopics = (request,response,next) =>{
+    return getQueryPerm('topics',{},{returning:["slug", "description"]})
+    .then((perm)=>{
+        return selectAllTopics(request.query,perm)
+    })
+    .then(({rows}) =>{
+        response.status(200).send(rows)
+    })
+    .catch((err)=>{
+        next(err)
+    })
+    
+}
